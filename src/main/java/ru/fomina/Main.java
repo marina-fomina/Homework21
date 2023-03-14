2a19c9ffcf4f38f6f56d2195f1e77dad3158e232
msfom <marinafomina1994@gmail.com>
2023-03-14 14:52:21 +0500

    ДЗ_JDBC (с изменениями)

--- src/main/java/ru/fomina/Main.java
+++ src/main/java/ru/fomina/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
             employeeDAO.addEmployee(employee);
 
-            employeeDAO.getById(2);
+            System.out.println(employeeDAO.getById(2));
 
             List<Employee> list = new ArrayList<>(employeeDAO.getAllEmployees());
             for(Employee empl : list){
